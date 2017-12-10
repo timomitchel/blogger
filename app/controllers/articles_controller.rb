30b1ced3e1b8c6faa08f218b47c5dec0499e99bc
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-10 12:39:32 -0700

    edit and flash implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,14 @@
 
 
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:show, :destroy, :edit, :update]
 
   def index
     @articles = Article.all
   end
 
   def show
-    @articles = Article.find(params[:id])
+
   end
 
   def new
@@ -18,19 +18,34 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' Created!"
     redirect_to action: "index"
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article '#{@article.title}' Delete!"
     redirect_to articles_path
+  end
+
+  def edit
+    
+  end
+
+  def update
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
